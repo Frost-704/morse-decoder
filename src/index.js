e4be14ac90cfeb30b0fa20c23d7d1f8eca23bc16
Frost-704 <boolean.pg.me@gmail.com>
2023-10-18 18:52:47 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,52 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let letters = [];
+    let finish = '';
+    let morse = [];
+    let start = 0;
+    let num = 10;
+    let letter = '';
+
+    for (let start = 0; start < expr.length; start += 10) {
+        letters.push(expr.slice(start, start + 10));
+    }
+
+    result = letters.map(function (e) {
+        if (e.startsWith('*')) {
+            return ' ';
+        }
+        return parseInt(e, 10).toString();
+    })
+
+    for (let k = 0; k < result.length; k += 1) {
+        let morseLetter = '';
+        if (result[k] === ' ') {
+            morseLetter += ' ';
+        } else {
+            for (let j = 0; j < result[k].length; j += 2) {
+                if (`${result[k][j]}${result[k][j + 1]}` === '10') {
+                    morseLetter += '.';
+                } else {
+                    morseLetter += '-';
+                }
+            }
+        }
+        morse.push(morseLetter);
+    }
+
+  for (let mor of morse) {
+        if (mor === ' ') {
+            finish += ' ';
+        } else {
+            finish += MORSE_TABLE[mor];
+        }
+    }
+    console.log(finish);
+    return finish;
 }
+
 
 module.exports = {
     decode
